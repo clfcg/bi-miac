968d657d4ab9b844444704efa6433c1ba7deb739
clfcg <leontev_dma@mail.ru>
2023-02-16 11:40:00 +0300

    feat: added jinja2 lib, templates dir, modul render_templates

---
+++ bi_miac/bi_cmd/rend_temlates.py
@@ -0,0 +1,18 @@
+import jinja2
+
+from bi_miac.configs.base_conf import TEMPLATES_DIR
+
+
+def render_template(template_name: str, data: dict | None):
+    if data is None:
+        data = {}
+    template = _get_template_env().get_template(template_name)
+    render = template.render(**data).replace("\n", "")
+    render = render.replace("<br>", "\n")
+    return render
+
+
+def _get_template_env():
+    template_loader = jinja2.FileSystemLoader(searchpath=TEMPLATES_DIR)
+    env = jinja2.Environment(loader=template_loader)
+    return env
--- bi_miac/configs/base_conf.py
+++ bi_miac/configs/base_conf.py
@@ -2,3 +2,6 @@ from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+CONF_DIR = BASE_DIR / "configs"
+TEMPLATES_DIR = BASE_DIR / "bi_cmd" / "templates"
