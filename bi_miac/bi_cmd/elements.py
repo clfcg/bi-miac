91137224b3e8549ff8699839a97039f7088f4494
clfcg <leontev_dma@mail.ru>
2023-02-20 17:01:49 +0300

    feat: added Field_ class

--- bi_miac/bi_cmd/elements.py
+++ bi_miac/bi_cmd/elements.py
@@ -2,6 +2,69 @@ from dataclasses import dataclass
 
 
 @dataclass
+class Field_:
+    field_data: str | int | bool = None
+
+
+    def is_filled(self, context: str, data=None):
+        data = input(f"{context}: ")
+        if data == "":
+            print("\033[1;31mДанные не заполнены!\033[0m\n")
+            return self.is_filled(context)
+        else:
+            return data
+
+
+    def get_type_field(self, context: str, data: str | None = None):
+        type_dict = {
+            "varchar": "строковый",
+            "numeric": "числовой",
+            "datetime": "дата, время",
+            "date": "дата",
+        }
+        data = self.is_filled(context, data)
+        if data not in type_dict:
+            print("\033[1;31mДоступные типы данных\033[0m:",)
+            for k, v in type_dict.items():
+                print(f"\033[1;34m{k:15}\033[0m {v}")
+            print()
+            return self.get_type_field(context)
+        else:
+            return data
+
+
+    def get_length_field(
+            self, context: str, 
+            data_type: str, 
+            data: int | None = None):
+
+        if data_type in ["datetime", "date"]:
+            return None
+        else:
+            try:
+                data = int(self.is_filled(context, data))
+                return data
+            except:
+                print("\033[1;31mДоступен ввод только целого числа\033[0m:\n",)
+                return self.get_length_field(context, data_type)
+
+    
+    def get_fsure_field(self, context: str, data=None):
+        data = self.is_filled(context, data)
+        if data == "0":
+            return False
+        elif data == "1":
+            return True
+        else:
+            print("\033[1;31mДоступные ключи для ввода\033[0m:")
+            print(f"\033[1;34m0\033[0m\tтег необязательный")
+            print(f"\033[1;34m1\033[0m\tтег обязательный\n")
+            return self.get_fsure_field(context)
+
+
+
+
+@dataclass
 class Tag:
     t_name: str
     t_type: str
@@ -13,4 +76,3 @@ class Tag:
 class Header:
     e_name: str
     tags: list[Tag]
-
--- bi_miac/bi_cmd/handlers/header_add_all.py
+++ bi_miac/bi_cmd/handlers/header_add_all.py
@@ -1,7 +1,28 @@
 import strictyaml as sy
 
-from bi_miac.bi_cmd.elements import Tag, Header
+from bi_miac.bi_cmd.elements import Field_, Tag, Header
 from bi_miac.bi_cmd.handlers.all_check_types import check_type, set_len, check_data_filled
+
+
+def get_header():
+    command = ""
+    element_name = Field_().is_filled("Имя элемента")
+
+    while command.lower() != "n":
+        tag_name = Field_().is_filled("Имя реквизита")
+        tag_type = Field_().get_type_field("Тип реквизита")
+        tag_length = Field_().get_length_field("Размер реквизита", tag_type)
+        for_sure = Field_().get_fsure_field("Обязательность реквизита(0-нет, 1-да)")
+        
+        command = input("Добавить еще реквизит? [y/n]: ")
+    
+    print(element_name, tag_name, tag_type, tag_length, for_sure)
+    
+
+get_header()
+
+
+
 
 
 def get_all_header():
@@ -26,6 +47,3 @@ def get_all_header():
     
     header = Header(elem_name, tag_list)
     print(header)
-
-
-get_all_header()
